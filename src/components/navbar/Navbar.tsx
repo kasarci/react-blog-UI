9ca36ee17544878e06dbf477c21428fe7632b05e
muhka1 <muhammedkasarci@hotmail.com>
2022-12-02 16:10:42 +0100

    Changed the order of the items in the Navbar.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -23,7 +23,10 @@ function Navbar({}: Props) {
 
   const SearchBox = styled(Box)({
     display: 'flex',
-    gap: 5
+    padding: '0',
+    margin: '0',
+    marginBottom: '100',
+    gap: 30
   }) as typeof Box;
 
   interface MenuItem {
@@ -42,26 +45,31 @@ function Navbar({}: Props) {
     <AppBar position='static' sx={{ background: 'black' }}>
       <StyledToolbar>
         
-        <SocialBox >
-          <LinkedIn />
-          <GitHub />
-        </SocialBox>
-        
-        <MenuBox sx={{display: {xs: 'none', sm:'none', md:'flex'}}}>
+        <MenuBox sx={{display: {xs: 'none', sm:'none', md:'flex', justifyItems: 'center', alignItems:'center'}}}>
           {
             MenuItems.map((item : MenuItem) => 
               <Typography sx={{ cursor: 'pointer', fontSize: '14px' }}>{item.Name}</Typography>
             )
           }
+          <SearchBox>
+            <InputBase placeholder='Search...' sx={{color: 'white'}} />
+          </SearchBox>
         </MenuBox>
         
-        <SearchBox>
-          <InputBase placeholder='Search...' sx={{color: 'white'}} />
-        </SearchBox>
 
         <MenuIcon 
           onClick={() => {setOpen(true)}}
           sx={{color:'white', cursor: 'pointer', display:{xs:'block', sm:'block', md: 'none'}}} />
+     
+        <SearchBox sx={{display: {xs:'block', sm:'block', md: 'none'}}}>
+            <InputBase placeholder='Search...' sx={{color: 'white'}} />
+          </SearchBox>
+
+        <SocialBox >
+          <LinkedIn />
+          <GitHub />
+        </SocialBox>
+        
         <Menu
           id="demo-positioned-menu"
           aria-labelledby="demo-positioned-button"
@@ -69,7 +77,7 @@ function Navbar({}: Props) {
           onClose={() => {setOpen(!open)}}
           anchorOrigin={{
             vertical: 'top',
-            horizontal: 'right',
+            horizontal: 'left',
           }}
           transformOrigin={{
             vertical: 'top',
